b25cd84da09776a0dd49004444b9406d36252052
Manas Reddy Arumalla <manasreddyarumalla@gmail.com>
2026-08-28 11:43:47 +0200

    Raise default training iterations to 3000

--- src/openarm_mjlab/tasks/puck/rl_cfg.py
+++ src/openarm_mjlab/tasks/puck/rl_cfg.py
@@ -52,7 +52,7 @@ def openarm_puck_ppo_runner_cfg() -> RslRlOnPolicyRunnerCfg:
         experiment_name="openarm_puck",
         save_interval=100,
         num_steps_per_env=24,
-        max_iterations=2_000,
+        max_iterations=3_000,
     )
 
 
